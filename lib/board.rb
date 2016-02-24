1e29e233332db52a9060903781984d1df40c076c
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-02-24 19:50:44 +0900

    bug fix

--- lib/board.rb
+++ lib/board.rb
@@ -3,15 +3,16 @@ class Board
   def initialize n, m
     @n = n
     @m = m
-    @buf = [ ' ' * n ] * n
+    @buf = Array.new(@n) { ' ' * n }
   end
 
   def check num
-    re_h = /#{num}{#{@m}}/
-    re_v = /(#{num}[.¥n]{#{@n}}){#{@m}}/
-    re_d = /(#{num}[,¥n]{#{@n+1}}){#{@m}}/
-    re_r = /(#{num}[,¥n]{#{@n-1}}){#{@m}}/
-    str = buf.join("¥n")+' '*@n
+    #                                      n=5, m=3
+    re_h = /#{num}{#{@m}}/               # /○{3}/
+    re_v = /(#{num}.{#{@n}}){#{@m}}/m    # /(○.){5}/m
+    re_d = /(#{num}.{#{@n+1}}){#{@m}}/m  # /(○.){6}/m
+    re_r = /(#{num}.{#{@n-1}}){#{@m}}/m  # /(○.){4}/m
+    str = @buf.join("\n")+' '*@n
     case str when re_h, re_v, re_d, re_r then
       return true
     else
@@ -22,7 +23,7 @@ class Board
   def place n, m, i
     raise 'oob' if n >= @n
     raise 'oob' if m >= @n
-    raise 'already' if /0/ !~ @buf[n][m]
+    raise 'already' if @buf[n][m] != ' '
     @buf[n][m] = i.to_s
     raise 'win' if check(i)
   end
